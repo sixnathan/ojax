8c6056c7b0c398fc569465982bc00fea4000cb23
sixnathan <246586399+sixnathan@users.noreply.github.com>
2026-07-07 14:16:34 +0100

    port: dtype

---
+++ src/dtype.ml
@@ -0,0 +1,8 @@
+type t = F32 | F64 | I32 | I64 | Bool
+
+let short_name = function
+  | F32 -> "f32"
+  | F64 -> "f64"
+  | I32 -> "i32"
+  | I64 -> "i64"
+  | Bool -> "bool"
---
+++ src/dtype.mli
@@ -0,0 +1,3 @@
+type t = F32 | F64 | I32 | I64 | Bool
+
+val short_name : t -> string
